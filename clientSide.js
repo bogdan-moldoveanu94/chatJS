5c8efc0bda0fe22ee99c8bc3af924c8ba62805ed
unknown <cristian.schuszter@SD-L04.sharedimension.com>
2016-01-21 13:13:50 +0100

    Other minor fixes

--- clientSide.js
+++ clientSide.js
@@ -17,7 +17,7 @@ $('form').submit(function(){
 
 socket.on('chat message', function(msg){
   $('#messages').append($('<li class="chat-bubble">').text(msg));
-	$(document).scrollTop($(document).height());
+	$('.message-div').scrollTop($('.message-div').height());
 });
 
 
